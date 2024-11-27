17543434192deb355a1e9119826fd24b3cb778b0
1635042 <munibaliaqat1@gmail.com>
2024-11-27 18:40:37 +0100

    Board and Tile logic implementation

--- model/board.py
+++ model/board.py
@@ -1,5 +1,5 @@
-from model.tile import Tile
 import random
+from model.tile import Tile
 
 
 class Board:
@@ -12,14 +12,13 @@ class Board:
     def __init__(self, difficulty):
         if difficulty not in self.DIFFICULTY_SETTINGS:
             raise ValueError("Please choose 1, 2, or 3.")
-
+        
         self.size = self.DIFFICULTY_SETTINGS[difficulty]["size"]
         self.total_bombs = self.DIFFICULTY_SETTINGS[difficulty]["bombs"]
-        self.is_game_lost = False
         self.tiles = self._initialize_board()
+        self.is_game_lost = False
 
     def _initialize_board(self):
-        
         tiles = [[Tile() for _ in range(self.size)] for _ in range(self.size)]
         bomb_positions = random.sample(range(self.size * self.size), self.total_bombs)
 
@@ -27,20 +26,32 @@ class Board:
             row, col = divmod(pos, self.size)
             tiles[row][col].is_bomb = True
 
+        for x in range(self.size):
+            for y in range(self.size):
+                tiles[x][y].neighboring_bombs = self._count_neighboring_bombs(tiles, x, y)
+
         return tiles
 
+    def _count_neighboring_bombs(self, tiles, x, y):
+        count = 0
+        for i in range(max(0, x - 1), min(self.size, x + 2)):
+            for j in range(max(0, y - 1), min(self.size, y + 2)):
+                if tiles[i][j].is_bomb:
+                    count += 1
+        return count
+
     def reveal_tile(self, x, y):
-        
-        tile = self.tiles[x][y]
-        tile.reveal()
-        if tile.is_bomb:
+        if self.tiles[x][y].is_bomb:
             self.is_game_lost = True
+        self.tiles[x][y].reveal()
 
     def is_game_won(self):
-        
         for row in self.tiles:
             for tile in row:
                 if not tile.is_bomb and not tile.is_revealed:
                     return False
         return True
 
+    def display_board(self):
+        for row in self.tiles:
+            print(" ".join(tile.display() for tile in row))
--- model/tile.py
+++ model/tile.py
@@ -1,11 +1,31 @@
 class Tile:
     def __init__(self):
-        self.is_bomb = False
-        self.is_revealed = False
-        self.neighboring_bombs = 0
+        self._is_bomb = False
+        self._is_revealed = False
+        self._neighboring_bombs = 0
+
+    @property
+    def is_bomb(self):
+        return self._is_bomb
+
+    @is_bomb.setter
+    def is_bomb(self, value):
+        self._is_bomb = value
+
+    @property
+    def is_revealed(self):
+        return self._is_revealed
 
     def reveal(self):
-        self.is_revealed = True
+        self._is_revealed = True
+
+    @property
+    def neighboring_bombs(self):
+        return self._neighboring_bombs
+
+    @neighboring_bombs.setter
+    def neighboring_bombs(self, value):
+        self._neighboring_bombs = value
 
     def display(self):
         if self.is_revealed:
